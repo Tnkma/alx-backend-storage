e44875438730423072e8c7a55f629e0b7c607c70
Godswill <onwusilikegodswill@gmail.com>
2024-07-23 17:49:51 +0100

    best band ever

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,13 @@
+DELIMITER //
+
+CREATE TRIGGER after_order_insert
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    -- Decrease the quantity of the item in the items table
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END //
+
+DELIMITER ;
