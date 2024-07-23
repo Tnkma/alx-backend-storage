6ea2d3906873be7aca9f560850519c7aafb11cc2
Godswill <onwusilikegodswill@gmail.com>
2024-07-23 17:30:03 +0100

    best band ever

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -2,7 +2,7 @@
 -- The following SQL script ranks countries by the number of fans
 
 SELECT origin,
-       SUM(nb_fans) AS total_fans
+       SUM(fans) AS total_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY total_fans DESC;
