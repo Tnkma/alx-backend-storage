65df07f17383f311f10340e01b8d113b2a7d20e8
Godswill <onwusilikegodswill@gmail.com>
2024-07-24 01:21:22 +0100

    setting up redis

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,64 +1,32 @@
-import requests
-import redis
+#!/usr/bin/env python3
+""" Redis Module """
+
 from functools import wraps
+import redis
+import requests
 from typing import Callable
 
-# Initialize Redis client
-cache = redis.Redis()
+redis_ = redis.Redis()
 
 
-def get_page(url: str) -> str:
-    """ Fetches the content of a web page and caches it for 10 seconds """
-    cache_key = f"count:{url}"
-    cached_content_key = f"content:{url}"
+def count_requests(method: Callable) -> Callable:
+    """the counting requests decorator"""
+    @wraps(method)
+    def wrapper(url):  # sourcery skip: use-named-expression
+        """ count requests """
+        redis_.incr(f"count:{url}")
+        cached_html = redis_.get(f"cached:{url}")
+        if cached_html:
+            return cached_html.decode('utf-8')
+        html = method(url)
+        redis_.setex(f"cached:{url}", 10, html)
+        return html
 
-    # Increment the access count for the URL
-    cache.incr(cache_key)
-
-    # Try to get the cached content
-    cached_content = cache.get(cached_content_key)
-    if cached_content:
-        return cached_content.decode('utf-8')
-
-    # If not cached, fetch the content from the URL
-    response = requests.get(url)
-    content = response.text
-
-    # Cache the content with an expiration time of 10 seconds
-    cache.setex(cached_content_key, 10, content)
-
-    return content
-
-
-# Bonus: Implementing with decorators
-def cache_page(fn: Callable) -> Callable:
-    """ Caches the content of a web page for 10 seconds """
-    @wraps(fn)
-    def wrapper(url: str) -> str:
-        """ Wrapper function to cache the content of a web page """
-        cache_key = f"count:{url}"
-        cached_content_key = f"content:{url}"
-
-        # Increment the access count for the URL
-        cache.incr(cache_key)
-
-        # Try to get the cached content
-        cached_content = cache.get(cached_content_key)
-        if cached_content:
-            return cached_content.decode('utf-8')
-
-        # If not cached, fetch the content from the URL
-        content = fn(url)
-
-        # Cache the content with an expiration time of 10 seconds
-        cache.setex(cached_content_key, 10, content)
-
-        return content
     return wrapper
 
 
-@cache_page
-def get_page_with_decorator(url: str) -> str:
-    """ Fetches the content of a web page and caches it for 10 seconds """
-    response = requests.get(url)
-    return response.text
+@count_requests
+def get_page(url: str) -> str:
+    """ Get page count"""
+    req = requests.get(url)
+    return req.text
