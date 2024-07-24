10c49264f3d999cc5994653030ba6594c1efccff
Godswill <onwusilikegodswill@gmail.com>
2024-07-24 01:04:41 +0100

    setting up redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """ writing strings to Redis"""
 import redis
 import uuid
-from typing import Union
+from typing import Union, Optional, Callable
 
 
 class Cache:
@@ -17,3 +17,29 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] =
+            None) -> Union[str, bytes, int, float, None]:
+        """ get data from redis
+
+        Args:
+            key (str): the key to get
+            fn (Optional[Callable], optional): used to convert the data.
+
+        Returns:
+            Union[str, bytes, int, float, None]: _description_
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """ get string from redis """
+        return self.get(key, lambda d: d.decode('utf-8'))
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """ get int from redis """
+        return self.get(key, lambda d: int(d))
