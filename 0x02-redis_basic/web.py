ad99c8671a057507159d66b3495ab004e45470a4
Godswill <onwusilikegodswill@gmail.com>
2024-07-27 20:53:07 +0100

    caching

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,37 +6,18 @@ import requests
 from typing import Callable
 from datetime import timedelta
 
-redis_ = redis.Redis()
 
-
-def count_requests(method: Callable) -> Callable:
-    """the counting requests decorator"""
-    @wraps(method)
-    def wrapper(url):
-        """ count requests """
-        redis_.incr(f"count:{url}")
-        cached_html = redis_.get(f"cached:{url}")
-        if cached_html:
-            return cached_html.decode('utf-8')
-        html = method(url)
-        redis_.setex(f"cached:{url}", 10, html)
-        return html
-
-    return wrapper
-
-
-@count_requests
 def get_page(url: str) -> str:
     """ Get page count"""
     if url is None or len(url.strip()) == 0:
         return ''
-    redis_store = redis.Redis()
+    redis = redis.Redis()
     res_key = 'result:{}'.format(url)
     req_key = 'count:{}'.format(url)
-    result = redis_store.get(res_key)
+    result = redis.get(res_key)
     if result is not None:
-        redis_store.incr(req_key)
+        redis.incr(req_key)
         return result
     result = requests.get(url).content.decode('utf-8')
-    redis_store.setex(res_key, timedelta(seconds=10), result)
+    redis.setex(res_key, timedelta(seconds=10), result)
     return result
