f0bcd210a8a361fd23658a5c992cbacdb5af0bcb
Godswill <onwusilikegodswill@gmail.com>
2024-07-27 15:59:19 +0100

    comments on each scripts

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,32 +1,17 @@
 -- SQL script that creates a stored procedure ComputeAverageWeightedScoreForUser that computes and store the average weighted score for a student.
 -- SQL script that creates a stored procedure ComputeAverageWeightedScoreForUser that computes and store the average weighted score for a student.
 
-DELIMITER
-
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
+DELIMITER |
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-    DECLARE done INT DEFAULT 0;
-    DECLARE userId INT;
-    DECLARE userCursor CURSOR FOR SELECT id FROM users;
-    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = 1;
-    
-    OPEN userCursor;
-    user_loop: LOOP
-        FETCH userCursor INTO userId;
-        IF done THEN
-            LEAVE user_loop;
-        END IF;
-        
-        UPDATE users u
-        SET u.average_score = (
-            SELECT IFNULL(SUM(c.score * p.weight) / SUM(p.weight), 0)
-            FROM corrections c
-            JOIN projects p ON c.project_id = p.id
-            WHERE c.user_id = userId
-        )
-        WHERE u.id = userId;
-    END LOOP;
-    CLOSE userCursor;
-END
-
-DELIMITER ;
+  UPDATE users AS U, 
+    (SELECT U.id, SUM(score * weight) / SUM(weight) AS w_avg 
+    FROM users AS U 
+    JOIN corrections as C ON U.id=C.user_id 
+    JOIN projects AS P ON C.project_id=P.id 
+    GROUP BY U.id)
+  AS WA
+  SET U.average_score = WA.w_avg 
+  WHERE U.id=WA.id;
+END;
