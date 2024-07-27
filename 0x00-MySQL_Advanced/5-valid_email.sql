a31a0feec7f61c8fc6550a7d8c5d68e43359aaaa
Godswill <onwusilikegodswill@gmail.com>
2024-07-27 16:04:58 +0100

    comments on each scripts

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,11 +1,12 @@
 -- reset valid email before update
 
 
-CREATE TRIGGER reset_valid_email_before_update
-BEFORE UPDATE ON users
+DELIMITER $$ ;
+CREATE TRIGGER validate BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-    IF NEW.email <> OLD.email THEN
-        SET NEW.valid_email = FALSE;
-    END IF;
-END
+	IF NEW.email != OLD.email THEN
+		SET NEW.valid_email = 0;
+	END IF;
+END;$$
+delimiter ;
--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,19 +1,16 @@
 -- script that creates a stored procedure AddBonus that adds a new correction for a student.
 
-DROP PROCEDURE IF EXISTS AddBonus;
-DELIMITER $$
+DELIMITER $$ ;
 CREATE PROCEDURE AddBonus(
-    IN user_id INT, 
-    IN project_name VARCHAR(255), 
-    IN score FLOAT)
+	IN user_id INTEGER,
+	IN project_name VARCHAR(255),
+	IN score INTEGER
+)
 BEGIN
-    DECLARE project_id INT;
-    IF (SELECT COUNT(*) FROM projects WHERE name = project_name) = 0
-    THEN
-        INSERT INTO projects (name) VALUES (project_name);
-    END IF;
-    SET project_id = (SELECT id FROM projects WHERE name = project_name LIMIT 1);
-    INSERT INTO corrections (user_id, project_id, score) VALUES(user_id, project_id, score);
-END
-
+	IF NOT EXISTS(SELECT name FROM projects WHERE name=project_name) THEN
+		INSERT INTO projects (name) VALUES (project_name);
+	END IF;
+	INSERT INTO corrections (user_id, project_id, score)
+	VALUES (user_id, (SELECT id from projects WHERE name=project_name), score);
+END;$$
 DELIMITER ;
