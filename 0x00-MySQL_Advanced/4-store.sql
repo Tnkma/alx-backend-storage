d6de6aef6d04de4c415ce3c84bfba26d50384b68
Godswill <onwusilikegodswill@gmail.com>
2024-07-27 14:12:19 +0100

    lists all abands with glam_rock

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,7 +1,15 @@
--- reates a trigger that decreases the quantity of an item after adding a new order.
+-- creates a trigger that decreses the quantity when a new insert is made
 
+DELIMITER //
 
-CREATE TRIGGER after_buy
+CREATE TRIGGER after_order_insert
 AFTER INSERT ON orders
 FOR EACH ROW
-UPDATE items SET quantity = quantity - NEW.number WHERE name = NEW.item_name;
+BEGIN
+    -- Decrease the quantity of the item in the items table
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name;
+END //
+
+DELIMITER ;
