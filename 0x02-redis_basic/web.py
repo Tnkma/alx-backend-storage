d5ccb6568ac88246bb9d138006979850322099da
Godswill <onwusilikegodswill@gmail.com>
2024-07-27 20:50:19 +0100

    caching

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """ Redis Module """
-
 from functools import wraps
 import redis
 import requests
 from typing import Callable
+from datetime import timedelta
 
 redis_ = redis.Redis()
 
@@ -12,7 +12,7 @@ redis_ = redis.Redis()
 def count_requests(method: Callable) -> Callable:
     """the counting requests decorator"""
     @wraps(method)
-    def wrapper(url):  # sourcery skip: use-named-expression
+    def wrapper(url):
         """ count requests """
         redis_.incr(f"count:{url}")
         cached_html = redis_.get(f"cached:{url}")
@@ -28,5 +28,15 @@ def count_requests(method: Callable) -> Callable:
 @count_requests
 def get_page(url: str) -> str:
     """ Get page count"""
-    req = requests.get(url)
-    return req.text
+    if url is None or len(url.strip()) == 0:
+        return ''
+    redis_store = redis.Redis()
+    res_key = 'result:{}'.format(url)
+    req_key = 'count:{}'.format(url)
+    result = redis_store.get(res_key)
+    if result is not None:
+        redis_store.incr(req_key)
+        return result
+    result = requests.get(url).content.decode('utf-8')
+    redis_store.setex(res_key, timedelta(seconds=10), result)
+    return result
